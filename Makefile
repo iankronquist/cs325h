d5923afe149717011249e82e3d8c08595f17aaf0
Ian Kronquist <iankronquist@gmail.com>
2016-01-12 18:50:48 -0800

    C++ version

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-CC=g++
-CFLAGS=-std=c99 -Werror -Wpedantic -Wall -Wextra
+CXX=g++
+CXXFLAGS=-std=c++98 -Werror -Wpedantic -Wall -Wextra
 
 all: tri_tiling
 
-tri_tiling: tri_tiling.c
-	${CC} tri_tiling.c -o tri_tiling ${CFLAGS}
+tri_tiling: tri_tiling.cpp
+	${CXX} tri_tiling.cpp -o tri_tiling ${CXXFLAGS}
+
+test_tri_tiling:
+	echo -e '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n-1' | ./tri_tiling
--- tri_tiling.cpp
+++ tri_tiling.cpp
@@ -1,25 +1,34 @@
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
+#include <string>
+
+using namespace std;
 
 int tri_tiling(int n);
 
 int main() {
-    char *line = NULL;
-    size_t linecap = 0;
-    long linelen;
-    while ((linelen = getline(&line, &linecap, stdin)) > 0) {
-        int n = atoi(line);
+    string num;
+    while (1) {
+        getline(cin, num);
+        int n = atoi(num.c_str());
         if (n == -1) {
             break;
         }
+        if (n & 1 || n == 0) {
+            cout << 0 << endl;
+            continue;
+        }
         int ans = tri_tiling(n);
-        fprintf(stdout, "%d\n", ans);
+        cout << ans << endl;
     }
-
+    return 0;
 }
 
 int tri_tiling(int n) {
-    int T[n][2];
+    int **T = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        T[i] = new int[2];
+    }
 
     // Base cases
     T[0][0] = 0;
@@ -30,7 +39,12 @@ int tri_tiling(int n) {
     for (int i = 2; i < n; ++i) {
         T[i][0] = 2*T[i-1][1] + T[i-2][0];
         T[i][1] = T[i-1][0] + T[i-2][1];
+
     }
     int ans =  T[n-1][0] > T[n-1][1] ? T[n-1][0] : T[n-1][1];
+    for (int i = 0; i < n; ++i) {
+        delete [] T[i];
+    }
+    delete [] T;
     return ans;
 }

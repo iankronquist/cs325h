b592854fdfe64ab8805d5063121ce0605b409792
Ian Kronquist <iankronquist@gmail.com>
2016-01-25 23:02:07 -0800

    Rewrite parts of to the max
    This includes the scanning and indexing code

--- tothemax.c
+++ tothemax.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,20 +6,26 @@ int find_max_sub_rectangle(int **matrix, int n);
 
 int main() {
     int n;
-    scanf("%*[ \n\t]%d", &n);
-    int **matrix = malloc(sizeof(int**)**sizeof(int**)n*n);
-    for (int x = 0; x < n*n; ++x) {
-        for (int y = 0; y < n*n; ++y) {
-            scanf("%*[ \n\t]%d", &matrix[x][y]);
+    scanf("%d", &n);
+    int **matrix = malloc(sizeof(int**)*n);
+    for (int i = 0; i < n; ++i) {
+        matrix[i] =  malloc(sizeof(int*)*n);
+    }
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n; ++y) {
+            scanf("%d", &matrix[x][y]);
         }
     }
-    int ans = find_max_sub_rectangle(matrix, n);
+    int ans = find_max_sub_rectangle((int**)matrix, n);
     printf("%d\n", ans);
     return 0;
 }
 
 int find_max_sub_rectangle(int **matrix, int n) {
-    int **table = malloc(sizeof(int**)**sizeof(int**)n*n);
+    int ***table = malloc(sizeof(int**)*n);
+    for (int i = 0; i < n; ++i) {
+        table[i] =  malloc(sizeof(int*)*n);
+    }
     int max = INT_MIN;
     for (int c = 0; c < n; ++c) {
         for (int r = 0; r < n; ++r) {
@@ -27,21 +34,47 @@ int find_max_sub_rectangle(int **matrix, int n) {
             if (table[c][r][0] > max) {
                 max = table[c][r][0];
             }
-            for (int h = 1; h < r; ++h) {
-                table[c][r][h] = table[c][r][h-1] + matrix[c][r];
+            int h;
+            for (h = 1; h <= r; ++h) {
+                table[c][r][h] = table[c][r][h-1] + matrix[c][r-h];
+                printf("%d %d %d = %d + %d\n", c, r, h, table[c][r][h-1], matrix[c][r-h]);
 
                 if (table[c][r][h] > max) {
                     max = table[c][r][h];
                 }
             }
             if (r > 0) {
-                for (h < (c+1)(r+1); ++h) {
-                    table[c][r][h] = table[c][r-1][h] + table[c][r][h];
+                // The indexing below here is goofed up
+                for (; h < (c+r); ++h) {
+                    table[c][r][h] = table[c][r-1][h] + table[c][r][h-1];
+                    printf("%d %d %d = c\n", c, r, h);
+                    //printf("%d %d %d = %d + %d\n", c, r, h, table[c][r-1][h], matrix[c][r-h]);
                     if (table[c][r][h] > max) {
                         max = table[c][r][h];
                     }
                 }
+                for (; h < (c+1)*(1+r); ++h) {
+                    table[c][r][h] = table[c][r][(c+1)*r] + table[c][r][r-h];
+                    printf("%d %d %d = c\n", c, r, h);
+                    //printf("%d %d %d = %d + %d\n", c, r, h, table[c][r-1][h], matrix[c][r-h]);
+                    if (table[c][r][h] > max) {
+                        max = table[c][r][h];
+                    }
+                }
+            } else {
+                printf("%d %d %d = ?\n", c, r, h);
             }
+        }
+    }
+    // Printing
+    for (int c = 0; c < n; ++c) {
+        for (int r = 0; r < n; ++r) {
+            int *arr = table[c][r];
+            printf("(%d, %d): ", c, r);
+            for (int h = 0; h < (c+1)*(r+1); ++h) {
+                printf("%d, ", arr[h]);
+            }
+            printf("\n");
         }
     }
     // Let the OS collect our garbage

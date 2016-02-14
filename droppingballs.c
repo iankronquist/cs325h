c4b1037ee10be93b0149a4ed35006cbf1426ee4b
Ian Kronquist <iankronquist@gmail.com>
2016-02-14 15:37:56 -0800

    Accepted version of dropping balls

--- droppingballs.c
+++ droppingballs.c
@@ -6,22 +6,21 @@ int drop_balls(int node, int balls, int max);
 int main() {
     int num_lines;
     scanf("%d", &num_lines);
-    for (int i = 0; i < num_lines; i++) {
+    int i;
+    for (i = 0; i < num_lines; i++) {
         int I, D;
-        scanf("%d %d", &I, &D);
-        // max = 2 raised to the D
-        int max = 1 << D;
-        printf("max, I, D %d %d %d\n", max, I, D);
+        scanf("%d %d", &D, &I);
+        /* max = 2 raised to the D */
+        int max = (1 << D)-1;
         printf("%d\n", drop_balls(1, I, max));
     }
 
     return 0;
 }
 
-/*
 int drop_balls(int node, int balls, int max) {
     int i;
-    for (i = 0; i < max && node*2 < max && node *2+1 < max; i++) {
+    for (i = 0; i < max && node*2 < max; i++) {
         if ((balls & 1) == 0) {
             node = node * 2 + 1;
             balls /= 2;
@@ -32,18 +31,17 @@ int drop_balls(int node, int balls, int max) {
     }
     return node;
 }
-*/
 
+/*
 // Recursive solution
 int drop_balls(int node, int balls, int max) {
     int left = node * 2;
-    int right = node * 2 + 1;
-    if (left < max && right < max) {
-        if ((balls & 1) == 0) { // Even
-            return drop_balls(right, balls/2, max);
-        } else { // Odd
-            return drop_balls(left, balls/2 + 1, max);
-        }
+    int right = left + 1;
+    if (left > max) return node;
+    if ((balls & 1) == 0) { // Even
+        return drop_balls(right, balls/2, max);
+    } else { // Odd
+        return drop_balls(left, balls/2 + 1, max);
     }
-    return node;
 }
+*/
